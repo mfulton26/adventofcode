11840946d85fe7a1ff2999ef59b03a786f05be46
Mark Fulton <mfulton26@gmail.com>
2020-07-04 20:04:45 -0500

    tester: decouple console logic

--- tester.js
+++ tester.js
@@ -23,13 +23,37 @@ const Status = {
 export async function test({ part } = {}) {
   const { year, day } = parsePathname(location.pathname);
   console.group(`Year ${year} Day ${day}`);
-  console.time("totalDuration");
+  console.time("elapsed");
+  const results = [];
   try {
-    const results = [];
     const parts = part !== undefined ? [part] : day === 25 ? [1] : [1, 2];
     for (const part of parts) {
-      for await (const result of testPart(year, day, part)) {
-        results.push(result);
+      console.group(`Part ${part}`);
+      try {
+        for await (const result of testPart(year, day, part)) {
+          if (result.end === undefined) {
+            console.group(result.name);
+          } else {
+            results.push(result);
+            const { status, reason } = result;
+            console.info("status:", status.name, status.circle);
+            console[status === Status.ERRED ? "error" : "info"](
+              "reason:",
+              ...reason
+            );
+            console.info(
+              "elapsed:",
+              `${result.elapsed.toLocaleString(undefined, {
+                minimumFractionDigits: 6,
+              })}ms`
+            );
+            console.groupEnd();
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        console.groupEnd();
       }
     }
     console.group("Test Summary");
@@ -42,7 +66,7 @@ export async function test({ part } = {}) {
     }
     console.groupEnd();
   } finally {
-    console.timeEnd("totalDuration");
+    console.timeEnd("elapsed");
     console.groupEnd();
   }
 }
@@ -53,43 +77,47 @@ export async function test({ part } = {}) {
  * @param {number} part
  */
 export async function* testPart(year, day, part) {
-  console.group(`Part ${part}`);
-  try {
-    /** @type {Array<{name?: string; input: string; expected: any}>} */
-    const testCases = await getTestCases(year, day, part);
-    const { solve } = await getSolver(year, day, part);
-    for (const { input, name = input, expected } of testCases) {
-      console.group(name);
-      console.time("duration");
-      try {
-        const actual = solve(input);
-        if (actual === expected) {
-          const status = Status.PASSED;
-          const reason = ["expected", expected, "and got", actual];
-          yield { name, status, reason };
-          console.info("status:", status.name, status.circle);
-          console.info("reason:", ...reason);
-        } else {
-          const status = Status.FAILED;
-          const reason = ["expected", expected, "but got", actual];
-          yield { name, status, reason };
-          console.info("status:", status.name, status.circle);
-          console.info("reason:", ...reason);
-        }
-      } catch (error) {
-        const status = Status.ERRED;
-        yield { name, status, reason: error };
-        console.info("status:", status.name, status.circle);
-        console.error("reason:", error);
-      } finally {
-        console.timeEnd("duration");
-        console.groupEnd();
+  /** @type {Array<{name?: string; input: string; expected: any}>} */
+  const testCases = await getTestCases(year, day, part);
+  const { solve } = await getSolver(year, day, part);
+  for (const { input, name = input, expected } of testCases) {
+    yield { name };
+    const start = performance.now();
+    try {
+      const actual = solve(input);
+      const end = performance.now();
+      const elapsed = end - start;
+      if (actual === expected) {
+        yield {
+          name,
+          status: Status.PASSED,
+          reason: ["expected", expected, "and got", actual],
+          start,
+          end,
+          elapsed,
+        };
+      } else {
+        yield {
+          name,
+          status: Status.FAILED,
+          reason: ["expected", expected, "but got", actual],
+          start,
+          end,
+          elapsed,
+        };
       }
+    } catch (error) {
+      const end = performance.now();
+      const elapsed = end - start;
+      yield {
+        name,
+        status: Status.ERRED,
+        reason: [error],
+        start,
+        end,
+        elapsed,
+      };
     }
-  } catch (error) {
-    console.error(error);
-  } finally {
-    console.groupEnd();
   }
 }
 
