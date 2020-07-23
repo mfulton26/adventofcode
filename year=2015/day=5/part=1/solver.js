a6282f1a84bfb7ff6394153dcac10b06404059c4
Mark Fulton <mfulton26@gmail.com>
2020-07-23 07:21:20 -0500

    2015/day/5: simplify counting

--- year=2015/day=5/part=1/solver.js
+++ year=2015/day=5/part=1/solver.js
@@ -1,15 +1,9 @@
-const isNiceRegExp = /^(?=(?:.*[aeiou].*){3,})(?=.*(.)\1)(?!.*(?:ab|cd|pq|xy).*).*$/;
+const isNiceRegExp = /^(?=(?:.*[aeiou].*){3,})(?=.*(.)\1)(?!.*(?:ab|cd|pq|xy).*).*$/gm;
 
 /**
  * @param {string} input
  * @returns {number}
  */
 export function solve(input) {
-  let count = 0;
-  for (const string of input.split("\n")) {
-    if (isNiceRegExp.test(string)) {
-      count++;
-    }
-  }
-  return count;
+  return input.match(isNiceRegExp)?.length ?? 0;
 }
--- year=2015/day=5/part=2/solver.js
+++ year=2015/day=5/part=2/solver.js
@@ -1,15 +1,9 @@
-const isNiceRegExp = /^(?=.*(.).\1.*)(?=.*(..).*\2).*$/;
+const isNiceRegExp = /^(?=.*(.).\1.*)(?=.*(..).*\2).*$/gm;
 
 /**
  * @param {string} input
  * @returns {number}
  */
 export function solve(input) {
-  let count = 0;
-  for (const string of input.split("\n")) {
-    if (isNiceRegExp.test(string)) {
-      count++;
-    }
-  }
-  return count;
+  return input.match(isNiceRegExp)?.length ?? 0;
 }
