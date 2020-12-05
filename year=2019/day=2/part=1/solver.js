bb9d1e75f2d86dd500f74f70ee059b6cb0cd776c
Mark Fulton <mfulton26@gmail.com>
2020-12-05 13:41:09 -0600

    Year 2019 Day 2 (revisit)

--- year=2019/day=2/part=1/solver.js
+++ year=2019/day=2/part=1/solver.js
@@ -1,16 +1,11 @@
-const defaultMemoryReplacements = /** @type {[number, number][]} */ (Object.freeze(
-  [Object.freeze([1, 12]), Object.freeze([2, 2])]
-));
-
-/**
- * @param {string} input
- * @param {object} [options]
- * @param {readonly (readonly [number, number])[]} [options.memoryReplacements]
- * @returns {number}
- */
 export function solve(
   input,
-  { memoryReplacements = defaultMemoryReplacements } = {}
+  {
+    memoryReplacements = [
+      [1, 12],
+      [2, 2],
+    ],
+  } = {}
 ) {
   const memory = input.split(",").map(Number);
   for (const [position, value] of memoryReplacements) {
@@ -20,10 +15,6 @@ export function solve(
   return memory[0];
 }
 
-/**
- * @param {number[]} memory
- * @returns {number[]}
- */
 export function runProgram(memory) {
   for (let instructionPointer = 0; memory[instructionPointer] !== 99; ) {
     const opcode = memory[instructionPointer++];
--- year=2019/day=2/part=2/solver.js
+++ year=2019/day=2/part=2/solver.js
@@ -1,9 +1,3 @@
-const DESIRED_OUTPUT = 19690720;
-
-/**
- * @param {string} input
- * @returns {number}
- */
 export function solve(input) {
   const initialMemory = input.split(",").map(Number);
   for (let noun = 0; noun <= 99; noun++) {
@@ -12,20 +6,13 @@ export function solve(input) {
       memory[1] = noun;
       memory[2] = verb;
       runProgram(memory);
-      if (memory[0] === DESIRED_OUTPUT) {
+      if (memory[0] === 19690720) {
         return 100 * noun + verb;
       }
     }
   }
-  throw new Error(
-    `no input noun and verb found to cause the program to produce the output ${DESIRED_OUTPUT}`
-  );
 }
 
-/**
- * @param {number[]} memory
- * @returns {number[]}
- */
 export function runProgram(memory) {
   for (let instructionPointer = 0; memory[instructionPointer] !== 99; ) {
     const opcode = memory[instructionPointer++];
