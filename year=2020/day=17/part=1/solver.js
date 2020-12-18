7ac888083ad13569ded2158d23fec7f13e836f67
Mark Fulton <mfulton26@gmail.com>
2020-12-17 22:55:52 -0600

    Year 2020 Day 17 (cleanup)

--- year=2020/day=17/part=1/solver.js
+++ year=2020/day=17/part=1/solver.js
@@ -1,10 +1,14 @@
-const neighborDeltas = [-1, 0, 1]
-  .flatMap((x, _, array) => array.flatMap((y) => array.map((z) => [x, y, z])))
-  .filter((coordinates) => coordinates.some((coordinate) => coordinate !== 0));
-
 export function solve(input) {
-  let activeCoordinatesSet = new Set(
-    input
+  let grid = parseGrid(input);
+  for (let n = 0; n < 6; n++) {
+    grid = cycle(grid);
+  }
+  return grid.size;
+}
+
+function parseGrid(text) {
+  return new Set(
+    text
       .split("\n")
       .flatMap((line, y) =>
         Array.from(line).flatMap((state, x) =>
@@ -12,39 +16,57 @@ export function solve(input) {
         )
       )
   );
-  for (let cycle = 0; cycle < 6; cycle++) {
-    const coordinatesHashMap = new Map(
-      Array.from(activeCoordinatesSet, (coordinates) => [
-        JSON.stringify(coordinates),
-        [coordinates, true],
-      ])
-    );
-    for (const [x, y, z] of activeCoordinatesSet) {
-      for (const [dx, dy, dz] of neighborDeltas) {
-        const coordinates = [x + dx, y + dy, z + dz];
-        const hash = JSON.stringify(coordinates);
-        if (!coordinatesHashMap.has(hash)) {
-          coordinatesHashMap.set(hash, [coordinates, false]);
-        }
+}
+
+function cycle(grid) {
+  const cycleMap = createCycleMap(grid);
+  const result = new Set();
+  for (const [coordinates, { active, activeNeighborCount }] of cycleMap) {
+    if (shouldBeActive(active, activeNeighborCount)) {
+      result.add(coordinates);
+    }
+  }
+  return result;
+}
+
+const neighborDeltas = [-1, 0, 1]
+  .flatMap((x, _, array) => array.flatMap((y) => array.map((z) => [x, y, z])))
+  .filter((coordinates) => coordinates.some((coordinate) => coordinate !== 0));
+
+function createCycleMap(grid) {
+  const hashMap = new Map(
+    Array.from(grid, (coordinates) => [
+      JSON.stringify(coordinates),
+      [coordinates, true],
+    ])
+  );
+  for (const [x, y, z] of grid) {
+    for (const [dx, dy, dz] of neighborDeltas) {
+      const coordinates = [x + dx, y + dy, z + dz];
+      const hash = JSON.stringify(coordinates);
+      if (!hashMap.has(hash)) {
+        hashMap.set(hash, [coordinates, false]);
       }
     }
-    activeCoordinatesSet = new Set(
-      Array.from(coordinatesHashMap.values()).flatMap(([[x, y, z], active]) => {
-        const activeNeighborCount = neighborDeltas.reduce(
-          (count, [dx, dy, dz]) =>
-            coordinatesHashMap.get(
-              JSON.stringify([x + dx, y + dy, z + dz])
-            )?.[1]
-              ? count + 1
-              : count,
-          0
-        );
-        active = active
-          ? activeNeighborCount === 2 || activeNeighborCount === 3
-          : activeNeighborCount === 3;
-        return active ? [[x, y, z]] : [];
-      })
-    );
   }
-  return activeCoordinatesSet.size;
+  const result = new Map();
+  for (const [coordinates, active] of hashMap.values()) {
+    const [x, y, z] = coordinates;
+    let activeNeighborCount = 0;
+    for (const [dx, dy, dz] of neighborDeltas) {
+      const hash = JSON.stringify([x + dx, y + dy, z + dz]);
+      const active = hashMap.get(hash)?.[1];
+      if (active) {
+        activeNeighborCount++;
+      }
+    }
+    result.set(coordinates, { active, activeNeighborCount });
+  }
+  return result;
+}
+
+function shouldBeActive(active, activeNeighborCount) {
+  return active
+    ? activeNeighborCount === 2 || activeNeighborCount === 3
+    : activeNeighborCount === 3;
 }
--- year=2020/day=17/part=2/solver.js
+++ year=2020/day=17/part=2/solver.js
@@ -1,12 +1,14 @@
-const neighborDeltas = [-1, 0, 1]
-  .flatMap((x, _, array) =>
-    array.flatMap((y) => array.flatMap((z) => array.map((w) => [x, y, z, w])))
-  )
-  .filter((coordinates) => coordinates.some((coordinate) => coordinate !== 0));
-
 export function solve(input) {
-  let activeCoordinatesSet = new Set(
-    input
+  let grid = parseGrid(input);
+  for (let n = 0; n < 6; n++) {
+    grid = cycle(grid);
+  }
+  return grid.size;
+}
+
+function parseGrid(text) {
+  return new Set(
+    text
       .split("\n")
       .flatMap((line, y) =>
         Array.from(line).flatMap((state, x) =>
@@ -14,41 +16,59 @@ export function solve(input) {
         )
       )
   );
-  for (let cycle = 0; cycle < 6; cycle++) {
-    const coordinatesHashMap = new Map(
-      Array.from(activeCoordinatesSet, (coordinates) => [
-        JSON.stringify(coordinates),
-        [coordinates, true],
-      ])
-    );
-    for (const [x, y, z, w] of activeCoordinatesSet) {
-      for (const [dx, dy, dz, dw] of neighborDeltas) {
-        const coordinates = [x + dx, y + dy, z + dz, w + dw];
-        const hash = JSON.stringify(coordinates);
-        if (!coordinatesHashMap.has(hash)) {
-          coordinatesHashMap.set(hash, [coordinates, false]);
-        }
+}
+
+function cycle(grid) {
+  const cycleMap = createCycleMap(grid);
+  const result = new Set();
+  for (const [coordinates, { active, activeNeighborCount }] of cycleMap) {
+    if (shouldBeActive(active, activeNeighborCount)) {
+      result.add(coordinates);
+    }
+  }
+  return result;
+}
+
+const neighborDeltas = [-1, 0, 1]
+  .flatMap((x, _, array) =>
+    array.flatMap((y) => array.flatMap((z) => array.map((w) => [x, y, z, w])))
+  )
+  .filter((coordinates) => coordinates.some((coordinate) => coordinate !== 0));
+
+function createCycleMap(grid) {
+  const hashMap = new Map(
+    Array.from(grid, (coordinates) => [
+      JSON.stringify(coordinates),
+      [coordinates, true],
+    ])
+  );
+  for (const [x, y, z, w] of grid) {
+    for (const [dx, dy, dz, dw] of neighborDeltas) {
+      const coordinates = [x + dx, y + dy, z + dz, w + dw];
+      const hash = JSON.stringify(coordinates);
+      if (!hashMap.has(hash)) {
+        hashMap.set(hash, [coordinates, false]);
       }
     }
-    activeCoordinatesSet = new Set(
-      Array.from(coordinatesHashMap.values()).flatMap(
-        ([[x, y, z, w], active]) => {
-          const activeNeighborCount = neighborDeltas.reduce(
-            (count, [dx, dy, dz, dw]) =>
-              coordinatesHashMap.get(
-                JSON.stringify([x + dx, y + dy, z + dz, w + dw])
-              )?.[1]
-                ? count + 1
-                : count,
-            0
-          );
-          active = active
-            ? activeNeighborCount === 2 || activeNeighborCount === 3
-            : activeNeighborCount === 3;
-          return active ? [[x, y, z, w]] : [];
-        }
-      )
-    );
   }
-  return activeCoordinatesSet.size;
+  const result = new Map();
+  for (const [coordinates, active] of hashMap.values()) {
+    const [x, y, z, w] = coordinates;
+    let activeNeighborCount = 0;
+    for (const [dx, dy, dz, dw] of neighborDeltas) {
+      const hash = JSON.stringify([x + dx, y + dy, z + dz, w + dw]);
+      const active = hashMap.get(hash)?.[1];
+      if (active) {
+        activeNeighborCount++;
+      }
+    }
+    result.set(coordinates, { active, activeNeighborCount });
+  }
+  return result;
+}
+
+function shouldBeActive(active, activeNeighborCount) {
+  return active
+    ? activeNeighborCount === 2 || activeNeighborCount === 3
+    : activeNeighborCount === 3;
 }
