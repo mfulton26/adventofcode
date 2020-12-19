a1a66e210746f9ee7605e26acf7a8aea94920417
Mark Fulton <mfulton26@gmail.com>
2020-12-19 15:17:21 -0600

    Year 2015 Day 4 (cleanup)

--- year2015/day4/part1/solver.js
+++ year2015/day4/part1/solver.js
@@ -1,10 +1,5 @@
 import md5 from "../../../crypto/md5.js";
 
-/**
- * @param {string} input
- * @param {string} [prefix]
- * @returns {number}
- */
 export function solve(input, prefix = "00000") {
   for (let number = 1; ; number++) {
     const hash = md5(`${input}${number}`);
