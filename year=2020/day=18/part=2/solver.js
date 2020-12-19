66b09cbd83b974d977cf27a7199d61f92203350d
Mark Fulton <mfulton26@gmail.com>
2020-12-18 20:57:50 -0600

    Year 2020 Day 18

---
+++ year=2020/day=18/part=2/solver.js
@@ -0,0 +1,23 @@
+export function solve(input) {
+  const expressions = input.split("\n");
+  return expressions.reduce((sum, expression) => sum + evaluate(expression), 0);
+}
+
+function evaluate(expression) {
+  let previous;
+  do {
+    previous = expression;
+    expression = expression.replace(/\(([^()]*?)\)/g, (_, sub) =>
+      evaluate(sub)
+    );
+  } while (expression !== previous);
+  do {
+    previous = expression;
+    expression = expression.replace(/\d+ [+] \d+/, eval);
+  } while (expression !== previous);
+  do {
+    previous = expression;
+    expression = expression.replace(/\d+ [*] \d+/, eval);
+  } while (expression !== previous);
+  return eval(expression);
+}
