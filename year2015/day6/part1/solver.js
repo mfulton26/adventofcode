6c2dfbbf6561650f7e728f0215929f1e387bf59c
Mark Fulton <mfulton26@gmail.com>
2020-12-24 11:41:15 -0600

    Year 2016 Day 6 (revisit)

--- year2015/day6/part1/solver.js
+++ year2015/day6/part1/solver.js
@@ -1,36 +1,44 @@
-import { parseInstructions } from "../instructionsParser.js";
-
-/**
- * @param {string} input
- * @returns {number}
- */
 export function solve(input) {
-  const lights = Array.from({ length: 1000 }, () =>
-    Array.from({ length: 1000 }, () => false)
-  );
+  const lights = Array.from({ length: 1000 }, () => Array(1000).fill(false));
+  const instructions = parseInstructions(input);
+  followInstructions(instructions, lights);
+  return countLit(lights);
+}
 
-  for (const {
-    name,
-    clientRect: { left, top, right, bottom },
-  } of parseInstructions(input)) {
+function* parseInstructions(text) {
+  for (const { groups } of text.matchAll(parseInstructions.regExp)) {
+    for (const name in parseInstructions.groupTypes) {
+      groups[name] = parseInstructions.groupTypes[name](groups[name]);
+    }
+    yield groups;
+  }
+}
+parseInstructions.regExp = /(?<name>turn on|turn off|toggle) (?<left>\d+),(?<top>\d+) through (?<right>\d+),(?<bottom>\d+)/g;
+parseInstructions.groupTypes = {
+  name: String,
+  left: Number,
+  top: Number,
+  right: Number,
+  bottom: Number,
+};
+
+function followInstructions(instructions, lights) {
+  for (const { name, left, top, right, bottom } of instructions) {
     for (let rowIndex = top; rowIndex <= bottom; rowIndex++) {
       const row = lights[rowIndex];
       for (let columnIndex = left; columnIndex <= right; columnIndex++) {
-        switch (name) {
-          case "turn on":
-            row[columnIndex] = true;
-            break;
-          case "turn off":
-            row[columnIndex] = false;
-            break;
-          case "toggle":
-            row[columnIndex] = !row[columnIndex];
-            break;
-        }
+        row[columnIndex] = followInstructions.functions[name](row[columnIndex]);
       }
     }
   }
+}
+followInstructions.functions = {
+  "turn on": () => true,
+  "turn off": () => false,
+  toggle: (light) => !light,
+};
 
+function countLit(lights) {
   let count = 0;
   for (const row of lights) {
     for (const light of row) {
--- year2015/day6/part2/solver.js
+++ year2015/day6/part2/solver.js
@@ -1,38 +1,44 @@
-import { parseInstructions } from "../instructionsParser.js";
-
-/**
- * @param {string} input
- * @returns {number}
- */
 export function solve(input) {
-  const lights = Array.from({ length: 1000 }, () =>
-    Array.from({ length: 1000 }, () => 0)
-  );
+  const lights = Array.from({ length: 1000 }, () => Array(1000).fill(0));
+  const instructions = parseInstructions(input);
+  followInstructions(instructions, lights);
+  return totalBrightness(lights);
+}
 
-  for (const {
-    name,
-    clientRect: { left, top, right, bottom },
-  } of parseInstructions(input)) {
+function* parseInstructions(text) {
+  for (const { groups } of text.matchAll(parseInstructions.regExp)) {
+    for (const name in parseInstructions.groupTypes) {
+      groups[name] = parseInstructions.groupTypes[name](groups[name]);
+    }
+    yield groups;
+  }
+}
+parseInstructions.regExp = /(?<name>turn on|turn off|toggle) (?<left>\d+),(?<top>\d+) through (?<right>\d+),(?<bottom>\d+)/g;
+parseInstructions.groupTypes = {
+  name: String,
+  left: Number,
+  top: Number,
+  right: Number,
+  bottom: Number,
+};
+
+function followInstructions(instructions, lights) {
+  for (const { name, left, top, right, bottom } of instructions) {
     for (let rowIndex = top; rowIndex <= bottom; rowIndex++) {
       const row = lights[rowIndex];
       for (let columnIndex = left; columnIndex <= right; columnIndex++) {
-        switch (name) {
-          case "turn on":
-            row[columnIndex] += 1;
-            break;
-          case "turn off":
-            if (row[columnIndex] > 0) {
-              row[columnIndex] -= 1;
-            }
-            break;
-          case "toggle":
-            row[columnIndex] += 2;
-            break;
-        }
+        row[columnIndex] = followInstructions.functions[name](row[columnIndex]);
       }
     }
   }
+}
+followInstructions.functions = {
+  "turn on": (light) => light + 1,
+  "turn off": (light) => (light ? light - 1 : light),
+  toggle: (light) => light + 2,
+};
 
+function totalBrightness(lights) {
   let total = 0;
   for (const row of lights) {
     for (const light of row) {
