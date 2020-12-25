7711e3c400fe6687afcb2cc1c715895dfb2d9679
Mark Fulton <mfulton26@gmail.com>
2020-12-25 12:13:37 -0600

    Year 2015 Day 9

---
+++ year2015/day9/part2/solver.js
@@ -0,0 +1,37 @@
+import { permute } from "../../../arrays/permuter.js";
+
+export function solve(input) {
+  const distances = parseDistances(input);
+  let longestDistance = -Infinity;
+  for (const route of permute(Array.from(distances.keys()))) {
+    let routeDistance = 0;
+    for (let i = 0, j = 1; j < route.length; i++, j++) {
+      const origin = route[i];
+      const destination = route[j];
+      const distance = distances.get(origin).get(destination);
+      routeDistance += distance;
+    }
+    if (routeDistance > longestDistance) {
+      longestDistance = routeDistance;
+    }
+  }
+  return longestDistance;
+}
+
+function parseDistances(text) {
+  const result = new Map();
+  function set(origin, destination, distance) {
+    if (!result.has(origin)) {
+      result.set(origin, new Map());
+    }
+    result.get(origin).set(destination, distance);
+  }
+  for (const line of text.split("\n")) {
+    const [lhs, rhs] = line.split(" = ");
+    const [origin, destination] = lhs.split(" to ");
+    const distance = Number(rhs);
+    set(origin, destination, distance);
+    set(destination, origin, distance);
+  }
+  return result;
+}
