8027bb8c6d5ca8034212cc65999d6e3477e7560a
Mark Fulton <mfulton26@gmail.com>
2020-12-25 11:41:45 -0600

    Year 2015 Day 5: reduce code

--- year2015/day5/part1/solver.js
+++ year2015/day5/part1/solver.js
@@ -1,13 +1,5 @@
 const isNiceRegExp = /^(?=(?:.*[aeiou].*){3,})(?=.*(.)\1)(?!.*(?:ab|cd|pq|xy).*).*$/gm;
 
 export function solve(input) {
-  return count(input.matchAll(isNiceRegExp));
-}
-
-function count(iterable) {
-  let count = 0;
-  for (const _ of iterable) {
-    count++;
-  }
-  return count;
+  return Array.from(input.matchAll(isNiceRegExp)).length;
 }
--- year2015/day5/part2/solver.js
+++ year2015/day5/part2/solver.js
@@ -1,13 +1,5 @@
 const isNiceRegExp = /^(?=.*(.).\1.*)(?=.*(..).*\2).*$/gm;
 
 export function solve(input) {
-  return count(input.matchAll(isNiceRegExp));
-}
-
-function count(iterable) {
-  let count = 0;
-  for (const _ of iterable) {
-    count++;
-  }
-  return count;
+  return Array.from(input.matchAll(isNiceRegExp)).length;
 }
