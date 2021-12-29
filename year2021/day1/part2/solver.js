f9921526b38bbb932e6486753d7017d3154b648f
Mark Fulton <mfulton26@gmail.com>
2021-12-29 10:19:08 -0600

    Year 2021 Day 1 (revisited)

--- year2021/day1/part2/solver.js
+++ year2021/day1/part2/solver.js
@@ -1,13 +1,10 @@
 import { parseDepths } from "../depthsParser.js";
 
 import count from "../../../lib/count.js";
-import sum from "../../../lib/sum.js";
 import windowed from "../../../lib/windowed.js";
 
 export function solve(input) {
   return parseDepths(input)
-    [windowed](3)
-    .map((window) => window[sum]())
-    [windowed](2)
-    [count](([a, b]) => b > a);
+    [windowed](4)
+    [count](([a, , , b]) => b > a);
 }
