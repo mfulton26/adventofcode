35c03fdc6671cc46c256d77aa03ee5698b065bcc
Mark Fulton <mfulton26@gmail.com>
2022-07-30 18:56:47 -0500

    feat: Year 2021 Day 13

---
+++ 2021/day/13/part/2/solve.ts
@@ -0,0 +1,27 @@
+export default function solve(input: string) {
+  const dots = Array.from(
+    input.matchAll(/(?<x>\d+),(?<y>\d+)/g),
+    ({ groups }) => ({ x: Number(groups!.x), y: Number(groups!.y) }),
+  );
+  const folds = Array.from(
+    input.matchAll(/(?<coordinate>[xy])=(?<position>\d+)/g),
+    ({ groups }) => ({
+      coordinate: groups!.coordinate as "x" | "y",
+      position: Number(groups!.position),
+    }),
+  );
+  const lengths = { x: Infinity, y: Infinity };
+  for (const { coordinate, position } of folds) {
+    for (const dot of dots) {
+      if (dot[coordinate] < position) continue;
+      dot[coordinate] = 2 * position - dot[coordinate];
+    }
+    lengths[coordinate] = position;
+  }
+  const paper = Array.from(
+    { length: lengths.y },
+    () => Array.from({ length: lengths.x }, () => "."),
+  );
+  for (const { x, y } of dots) paper[y][x] = "#";
+  return paper.map((line) => line.join("")).join("\n");
+}
