fb743571d7cbad11e5ced34cd7095b580381da22
Mark Fulton <mfulton26@gmail.com>
2022-08-06 08:33:42 -0500

    feat(template): improve test template
    in cases where both an "example" and single line inputs are desired the
    single line inputs likely come first (at least based on 2021/day/18)

--- template/part/1/solve.test.ts
+++ template/part/1/solve.test.ts
@@ -2,18 +2,17 @@ import solve from "./solve.ts";
 
 import { assertEquals } from "std/testing/asserts.ts";
 
-Deno.test("example", () => {
-  const input = ``;
-
-  assertEquals(solve(input), undefined);
-});
-
 Deno.test("one_line_input_1", expect(undefined));
-Deno.test("one_line_input_2", expect(undefined));
-Deno.test("one_line_input_3", expect(undefined));
+Deno.test("one_line_input_N", expect(undefined));
 
 function expect(expected: unknown) {
   return (t: Deno.TestContext) => {
     assertEquals(solve(t.name), expected);
   };
 }
+
+Deno.test("example", () => {
+  const input = ``;
+
+  assertEquals(solve(input), undefined);
+});
