a2ad32a6726fc22ce441d558f173c16faf3bccaa
Mark Fulton <mfulton26@gmail.com>
2022-08-13 08:04:02 -0500

    feat: Year 2015 Day 7

---
+++ 2015/day/7/part/1/solve.test.ts
@@ -0,0 +1,25 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("example", () => {
+  const input = `123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i`;
+
+  assertEquals(solve.all(input), {
+    "d": 72,
+    "e": 507,
+    "f": 492,
+    "g": 114,
+    "h": 65412,
+    "i": 65079,
+    "x": 123,
+    "y": 456,
+  });
+});
---
+++ 2015/day/7/part/1/solve.ts
@@ -0,0 +1,72 @@
+type Wire = (signals: { get(key: string): number }) => number;
+
+export default function solve(input: string) {
+  const instructions = parseInstructions(input);
+  const signals = createSignals(instructions);
+  return signals.get("a");
+}
+
+solve.all = (input: string) => {
+  const instructions = parseInstructions(input);
+  const signals = createSignals(instructions);
+  return Object.fromEntries(
+    Array.from(instructions.keys())
+      .sort()
+      .map((key) => [key, signals.get(key)]),
+  );
+};
+
+function parseInstructions(text: string) {
+  const parse = <Record<string, (words: string[]) => [string, Wire]>> {
+    "AND": ([a, , b, , destination]) => {
+      return [destination, (signals) => signals.get(a) & signals.get(b)];
+    },
+    "OR": ([a, , b, , destination]) => {
+      return [destination, (signals) => signals.get(a) | signals.get(b)];
+    },
+    "LSHIFT": ([a, , b, , destination]) => {
+      return [destination, (signals) => signals.get(a) << signals.get(b)];
+    },
+    "RSHIFT": ([a, , b, , destination]) => {
+      return [destination, (signals) => signals.get(a) >>> signals.get(b)];
+    },
+    "NOT": ([, source, , destination]) => {
+      return [destination, (signals) => ~signals.get(source) & 0xffff];
+    },
+    default: ([source, , destination]) => {
+      return [destination, (signals) => signals.get(source)];
+    },
+  };
+
+  function parseInstructionEntry(line: string) {
+    const words = line.split(" ");
+    if (words[0] === "NOT") return parse["NOT"](words);
+    switch (words[1]) {
+      case "AND":
+      case "OR":
+      case "LSHIFT":
+      case "RSHIFT":
+        return parse[words[1]](words);
+      default:
+        return parse.default(words);
+    }
+  }
+
+  function* parseInstructionEntries(text: string) {
+    for (const line of text.split("\n")) yield parseInstructionEntry(line);
+  }
+
+  return new Map(parseInstructionEntries(text));
+}
+
+function createSignals(instructions: Map<string, Wire>) {
+  const signals = new Map();
+  function get(identifier: string) {
+    if (/^\d+$/.test(identifier)) return Number(identifier);
+    if (!signals.has(identifier)) {
+      signals.set(identifier, instructions.get(identifier)!({ get }));
+    }
+    return signals.get(identifier);
+  }
+  return { get };
+}
