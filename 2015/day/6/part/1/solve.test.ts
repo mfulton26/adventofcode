b35a29671c0f98a54184861c62b0faa78c474ee8
Mark Fulton <mfulton26@gmail.com>
2022-08-13 07:23:43 -0500

    feat: Year 2015 Day 6

---
+++ 2015/day/6/part/1/solve.test.ts
@@ -0,0 +1,13 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("turn on 0,0 through 999,999", expect(1000000));
+Deno.test("toggle 0,0 through 999,0", expect(1000));
+Deno.test("turn off 499,499 through 500,500", expect(0));
+
+function expect(expected: unknown) {
+  return (t: Deno.TestContext) => {
+    assertEquals(solve(t.name), expected);
+  };
+}
---
+++ 2015/day/6/part/2/solve.test.ts
@@ -0,0 +1,12 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("turn on 0,0 through 0,0", expect(1));
+Deno.test("toggle 0,0 through 999,999", expect(2000000));
+
+function expect(expected: unknown) {
+  return (t: Deno.TestContext) => {
+    assertEquals(solve(t.name), expected);
+  };
+}
