a6b965a0457067db500c54545cc175579cdda7f8
Mark Fulton <mfulton26@gmail.com>
2022-08-13 16:02:55 -0500

    feat: Year 2015 Day 11

---
+++ 2015/day/11/part/1/solve.test.ts
@@ -0,0 +1,24 @@
+import solve, { isValidPassword } from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("isValidPassword", async (t) => {
+  await t.step("hijklmmn", expect(false));
+  await t.step("abbceffg", expect(false));
+  await t.step("abbcegjk", expect(false));
+
+  function expect(expected: unknown) {
+    return (t: Deno.TestContext) => {
+      assertEquals(isValidPassword(t.name), expected);
+    };
+  }
+});
+
+Deno.test("abcdefgh", expect("abcdffaa"));
+Deno.test("ghijklmn", expect("ghjaabcc"));
+
+function expect(expected: unknown) {
+  return (t: Deno.TestContext) => {
+    assertEquals(solve(t.name), expected);
+  };
+}
