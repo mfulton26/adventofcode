ab749509364246942b3145540325239240536b6f
Mark Fulton <mfulton26@gmail.com>
2022-08-13 16:58:00 -0500

    feat(2015/day/12): clean-up

--- 2015/day/12/part/2/solve.ts
+++ 2015/day/12/part/2/solve.ts
@@ -1,11 +1,11 @@
 export default function solve(input: string) {
-  input = JSON.stringify(
-    JSON.parse(input),
-    (_, value) =>
-      value.constructor === Object && Object.values(value).includes("red")
-        ? {}
-        : value,
-  );
-  const numbers = input.match(/-?\d+/g)?.map(Number) ?? [];
+  const numbers = JSON.stringify(JSON.parse(input), solve.replacer)
+    .match(/-?\d+/g)?.map(Number) ?? [];
   return numbers.reduce((sum, number) => sum + number, 0);
 }
+// fixme(typescript 4.8): use NonNullable<unknown> instead
+// deno-lint-ignore ban-types
+solve.replacer = (_: string, value: {}) =>
+  value.constructor === Object && Object.values(value).includes("red")
+    ? {}
+    : value;
