510de38ebf2b33d905d7d182e7ec3e94568402d3
Mark Fulton <mfulton26@gmail.com>
2022-08-13 16:05:53 -0500

    feat(2015/day/10): nest non-solve tests

--- 2015/day/10/part/1/solve.test.ts
+++ 2015/day/10/part/1/solve.test.ts
@@ -2,14 +2,16 @@ import { lookAndSay } from "./solve.ts";
 
 import { assertEquals } from "std/testing/asserts.ts";
 
-Deno.test("1", expect("11"));
-Deno.test("11", expect("21"));
-Deno.test("21", expect("1211"));
-Deno.test("1211", expect("111221"));
-Deno.test("111221", expect("312211"));
+Deno.test("lookAndSay", async (t) => {
+  await t.step("1", expect("11"));
+  await t.step("11", expect("21"));
+  await t.step("21", expect("1211"));
+  await t.step("1211", expect("111221"));
+  await t.step("111221", expect("312211"));
 
-function expect(expected: unknown) {
-  return (t: Deno.TestContext) => {
-    assertEquals(lookAndSay(t.name), expected);
-  };
-}
+  function expect(expected: unknown) {
+    return (t: Deno.TestContext) => {
+      assertEquals(lookAndSay(t.name), expected);
+    };
+  }
+});
