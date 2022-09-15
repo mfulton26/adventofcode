e242f4e36506223c8afe2f8c7c4392fa97013e75
Mark Fulton <mfulton26@gmail.com>
2022-09-14 19:18:25 -0500

    feat(2021/day/6): improve readability

--- 2021/day/6/part/2/solve.ts
+++ 2021/day/6/part/2/solve.ts
@@ -1,11 +1,11 @@
 export default function solve(input: string) {
   const ages = input.split(",").map(Number);
-  const timers = Array<number>(9).fill(0);
-  for (const age of ages) timers[age]++;
+  const timerCounts = Array<number>(9).fill(0);
+  for (const age of ages) timerCounts[age]++;
   for (let n = 0; n < 256; n++) {
-    const resetCount = timers.shift()!;
-    timers[6] += resetCount;
-    timers[8] = resetCount;
+    const resetCount = timerCounts.shift()!;
+    timerCounts[6] += resetCount;
+    timerCounts.push(resetCount);
   }
-  return timers.reduce((sum, n) => sum + n, 0);
+  return timerCounts.reduce((sum, n) => sum + n, 0);
 }
