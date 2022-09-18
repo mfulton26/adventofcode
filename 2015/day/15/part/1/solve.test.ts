8e2ef4df14f158fad2d35f88d00d6c7db9f6b805
Mark Fulton <mfulton26@gmail.com>
2022-09-17 20:22:05 -0500

    feat: 2015/day/15

---
+++ 2015/day/15/part/1/solve.test.ts
@@ -0,0 +1,11 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("example", () => {
+  const input = `\
+Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3`;
+
+  assertEquals(solve(input), 62842880);
+});
---
+++ 2015/day/15/part/2/solve.ts
@@ -0,0 +1,72 @@
+export default function solve(input: string) {
+  const table = parseTable(input);
+  const ingredients = Array.from(table.rowKeys());
+  const properties = Array.from(table.columnKeys()).filter(
+    (property) => property !== "calories",
+  );
+  let maxScore = -Infinity;
+  for (const recipe of recipes(ingredients)) {
+    const propertyTotal = (property: string) =>
+      Math.max(
+        ingredients.reduce(
+          (total, ingredient) =>
+            total + recipe.get(ingredient)! * table.get(ingredient, property)!,
+          0,
+        ),
+        0,
+      );
+    if (propertyTotal("calories") !== 500) {
+      continue;
+    }
+    const score = properties.reduce(
+      (score, property) => score * propertyTotal(property),
+      1,
+    );
+    if (score > maxScore) {
+      maxScore = score;
+    }
+  }
+  return maxScore;
+}
+
+function parseTable(text: string) {
+  const rows = new Map<string, Map<string, number>>();
+  const columnKeySet = new Set<string>();
+  for (const line of text.split("\n")) {
+    const [ingredient, propertiesText] = line.split(": ");
+    const row = new Map<string, number>();
+    for (const propertyText of propertiesText.split(", ")) {
+      const [property, teaspoonsText] = propertyText.split(" ");
+      const teaspoons = Number(teaspoonsText);
+      columnKeySet.add(property);
+      row.set(property, teaspoons);
+    }
+    rows.set(ingredient, row);
+  }
+  return {
+    rowKeys: () => rows.keys(),
+    columnKeys: () => columnKeySet.values(),
+    get: (rowKey: string, columnKey: string) =>
+      rows.get(rowKey)?.get(columnKey),
+  };
+}
+
+function* recipes(
+  ingredients: string[],
+  teaspoons = 100,
+): IterableIterator<Map<string, number>> {
+  switch (ingredients.length) {
+    case 0:
+      throw new Error("ingredients.length must be nonzero");
+    case 1:
+      yield new Map([[ingredients[0], teaspoons]]);
+      break;
+    default:
+      for (let amount = 0; amount <= teaspoons; amount++) {
+        const [ingredient, ...remaining] = ingredients;
+        for (const subRecipe of recipes(remaining, teaspoons - amount)) {
+          yield new Map([[ingredient, amount], ...subRecipe]);
+        }
+      }
+  }
+}
