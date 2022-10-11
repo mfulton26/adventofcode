0231ce8486c48b6efd00763fc323e4baf6e300d7
Mark Fulton <mfulton26@gmail.com>
2022-10-10 21:13:35 -0400

    feat: 2016/day/2

---
+++ 2016/day/2/part/1/solve.test.ts
@@ -0,0 +1,13 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("example", () => {
+  const input = `\
+ULL
+RRDDD
+LURDL
+UUUUD`;
+
+  assertEquals(solve(input), "1985");
+});
