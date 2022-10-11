149b7a29b0ab2a2f3b758029a79de383b02d8566
Mark Fulton <mfulton26@gmail.com>
2022-10-11 11:16:51 -0400

    feat: 2019/day/2

---
+++ 2019/day/2/part/1/solve.test.ts
@@ -0,0 +1,33 @@
+import solve, { runProgram } from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("1,9,10,3,2,3,11,0,99,30,40,50", expect(3500));
+Deno.test("1,0,0,0,99", expect(2));
+Deno.test("2,3,0,3,99", expect(2));
+Deno.test("2,4,4,5,99,0", expect(2));
+Deno.test("1,1,1,4,99,5,6,0,99", expect(30));
+
+function expect(expected: unknown) {
+  const options = { memoryReplacements: [] };
+  return (t: Deno.TestContext) => {
+    assertEquals(solve(t.name, options), expected);
+  };
+}
+
+Deno.test("runProgram", async (t) => {
+  await t.step(
+    "1,9,10,3,2,3,11,0,99,30,40,50",
+    expect([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]),
+  );
+  await t.step("1,0,0,0,99", expect([2, 0, 0, 0, 99]));
+  await t.step("2,3,0,3,99", expect([2, 3, 0, 6, 99]));
+  await t.step("2,4,4,5,99,0", expect([2, 4, 4, 5, 99, 9801]));
+  await t.step("1,1,1,4,99,5,6,0,99", expect([30, 1, 1, 4, 2, 5, 6, 0, 99]));
+
+  function expect(expected: unknown) {
+    return (t: Deno.TestContext) => {
+      assertEquals(runProgram(t.name.split(",").map(Number)), expected);
+    };
+  }
+});
---
+++ 2019/day/2/part/1/solve.ts
@@ -0,0 +1,31 @@
+export default function solve(
+  input: string,
+  { memoryReplacements = [[1, 12], [2, 2]] } = {},
+) {
+  const memory = input.split(",").map(Number);
+  for (const [position, value] of memoryReplacements) memory[position] = value;
+  runProgram(memory);
+  return memory[0];
+}
+
+export function runProgram(memory: number[]) {
+  for (let instructionPointer = 0; memory[instructionPointer] !== 99;) {
+    const opcode = memory[instructionPointer++];
+    const parameters = (function* () {
+      while (true) yield memory[instructionPointer++];
+    })();
+    switch (opcode) {
+      case 1: {
+        const [a, b, c] = parameters;
+        memory[c] = memory[a] + memory[b];
+        break;
+      }
+      case 2: {
+        const [a, b, c] = parameters;
+        memory[c] = memory[a] * memory[b];
+        break;
+      }
+    }
+  }
+  return memory;
+}
