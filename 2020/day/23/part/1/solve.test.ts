46963d923d983fc028a24e38573c301abf440bb8
Mark Fulton <mfulton26@gmail.com>
2022-10-12 00:43:53 -0400

    feat: 2020/day/23

---
+++ 2020/day/23/part/1/solve.test.ts
@@ -0,0 +1,11 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("389125467", expect("67384529"));
+
+function expect(expected: unknown) {
+  return (t: Deno.TestContext) => {
+    assertEquals(solve(t.name), expected);
+  };
+}
---
+++ 2020/day/23/part/2/solve.ts
@@ -0,0 +1,48 @@
+export default function solve(input: string, { moves = 10000000 } = {}) {
+  const cups = parseCups(input);
+  let [[currentCup]] = cups;
+  for (let move = 1; move <= moves; move++) {
+    const [a, b, c] = valuesAfter(cups, currentCup);
+    let destinationCup = currentCup;
+    destinationCup:
+    while (true) {
+      destinationCup = destinationCup === 1 ? cups.size : destinationCup - 1;
+      switch (destinationCup) {
+        case a:
+        case b:
+        case c:
+          continue destinationCup;
+        default:
+          break destinationCup;
+      }
+    }
+    cups.set(currentCup, cups.get(c)!);
+    cups.set(c, cups.get(destinationCup)!);
+    cups.set(destinationCup, a);
+    [currentCup] = valuesAfter(cups, currentCup);
+  }
+  const [a, b] = valuesAfter(cups, 1);
+  return a * b;
+}
+
+function parseCups(text: string) {
+  const labels = Array.from(text, Number);
+  for (let label = labels.length + 1; label <= 1000000; label++) {
+    labels.push(label);
+  }
+  const cups = new Map<number, number>();
+  for (let i = 0; i < labels.length; i++) {
+    cups.set(labels[i], labels[(i + 1) % labels.length]);
+  }
+  return cups;
+}
+
+function* valuesAfter(map: Map<number, number>, reference: number) {
+  for (
+    let value = map.get(reference)!;
+    value !== reference;
+    value = map.get(value)!
+  ) {
+    yield value;
+  }
+}
