503ea3cfadd7f3ada66ef3e24deee9e7eb656f8c
Mark Fulton <mfulton26@gmail.com>
2022-10-12 00:37:37 -0400

    feat: 2020/day/22

---
+++ 2020/day/22/part/2/solve.test.ts
@@ -0,0 +1,22 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("example", () => {
+  const input = `\
+Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10`;
+
+  assertEquals(solve(input), 291);
+});
---
+++ 2020/day/22/part/2/solve.ts
@@ -0,0 +1,48 @@
+export default function solve(input: string) {
+  const decks = parseDecks(input);
+  const winner = playRecursiveCombat(decks);
+  return findScore(decks[winner]);
+}
+
+function parseDecks(text: string) {
+  return text.split("\n\n")
+    .map((deckText) => deckText.split("\n").slice(1).map(Number));
+}
+
+function playRecursiveCombat(decks: number[][]) {
+  const seen = new Set<string>();
+  while (decks.every((deck) => deck.length)) {
+    const configuration = stringifyDecks(decks);
+    if (seen.has(configuration)) {
+      return 0;
+    }
+    seen.add(configuration);
+    const cards = decks.map((deck) => deck.shift()!);
+    if (cards.every((card, player) => decks[player].length >= card)) {
+      const winner = playRecursiveCombat(
+        decks.map((deck, player) => deck.slice(0, cards[player])),
+      );
+      const winningCard = cards[winner];
+      const losingCard = cards[winner === 0 ? 1 : 0];
+      decks[winner].push(winningCard, losingCard);
+    } else {
+      const [{ player: winner, card: winningCard }, { card: losingCard }] =
+        cards
+          .map((card, player) => ({ player, card }))
+          .sort(({ card: a }, { card: b }) => b - a);
+      decks[winner].push(winningCard, losingCard);
+    }
+  }
+  return decks.findIndex((deck) => deck.length);
+}
+
+function stringifyDecks(decks: number[][]) {
+  return decks.map((deck) => `${deck}`).join("\n");
+}
+
+function findScore(deck: number[]) {
+  return deck.reduce(
+    (score, card, index, { length }) => score + card * (length - index),
+    0,
+  );
+}
