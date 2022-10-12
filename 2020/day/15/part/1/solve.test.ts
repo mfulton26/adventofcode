4c01c1de1d00c791b37a51c66732a72a04ac5f90
Mark Fulton <mfulton26@gmail.com>
2022-10-11 22:47:35 -0400

    feat: 2020/day/15

---
+++ 2020/day/15/part/1/solve.test.ts
@@ -0,0 +1,17 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("0,3,6", expect(436));
+Deno.test("1,3,2", expect(1));
+Deno.test("2,1,3", expect(10));
+Deno.test("1,2,3", expect(27));
+Deno.test("2,3,1", expect(78));
+Deno.test("3,2,1", expect(438));
+Deno.test("3,1,2", expect(1836));
+
+function expect(expected: unknown) {
+  return (t: Deno.TestContext) => {
+    assertEquals(solve(t.name), expected);
+  };
+}
