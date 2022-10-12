387bac0de376e9311ca63d4018ce2aef27fcc2d3
Mark Fulton <mfulton26@gmail.com>
2022-10-11 22:22:44 -0400

    feat: 2020/day/10

---
+++ 2020/day/10/part/2/solve.test.ts
@@ -0,0 +1,58 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("example 1", () => {
+  const input = `\
+16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`;
+
+  assertEquals(solve(input), 8);
+});
+
+
+Deno.test("example 2", () => {
+  const input = `\
+28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3`;
+
+  assertEquals(solve(input), 19208);
+});
