201cce9a14891db1125a8e40124d577ca915c9e5
Mark Fulton <mfulton26@gmail.com>
2024-01-07 11:18:52 -0600

    feat(2022/day/7): simplify result calculation

--- 2022/day/7/part/2/solve.ts
+++ 2022/day/7/part/2/solve.ts
@@ -1,15 +1,13 @@
-import { type DirInfo, parseFromTerminalOutput } from "../../filesystem.ts";
+import { parseFromTerminalOutput } from "../../filesystem.ts";
 
 export default function solve(input: string) {
   const { root } = parseFromTerminalOutput(input);
   const unusedSpace = 70000000 - root.size;
   const spaceNeeded = 30000000 - unusedSpace;
-  const deletionCandidates: DirInfo[] = [];
-  for (const dir of root.dirIterator()) {
-    if (dir.size < spaceNeeded) continue;
-    deletionCandidates.push(dir);
+  let result = Infinity;
+  for (const { size } of root.dirIterator()) {
+    if (size < spaceNeeded || size >= result) continue;
+    result = size;
   }
-  const [deletionTarget] = deletionCandidates
-    .sort(({ size: a }, { size: b }) => a - b);
-  return deletionTarget.size;
+  return result;
 }
