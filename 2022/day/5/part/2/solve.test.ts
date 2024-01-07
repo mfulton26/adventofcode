727e412e9fbc9596cee71f59a89c665de794ac79
Mark Fulton <mfulton26@gmail.com>
2024-01-07 11:18:52 -0600

    feat: 2022 Day 5

---
+++ 2022/day/5/part/2/solve.test.ts
@@ -0,0 +1,18 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "../../../../../lib/testing/asserts.ts";
+
+Deno.test("example", () => {
+  const input = `\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+  assertEquals(solve(input), "MCD");
+});
---
+++ 2022/day/5/part/2/solve.ts
@@ -0,0 +1,35 @@
+function parseStartingStacks(text: string) {
+  const [idLine, ...crateLines] = text.split("\n").reverse();
+  const result = new Map<string, string[]>();
+  for (const { 0: id, index } of idLine.matchAll(/\S+/g)) {
+    const stack = crateLines
+      .map((line) => line.charAt(index!))
+      .filter((char) => char !== " ");
+    result.set(id, stack);
+  }
+  return result;
+}
+
+parseRearrangementProcedures.regexp =
+  /^move (?<quantityText>\d+) from (?<source>\d+) to (?<destination>\d+)$/gm;
+function* parseRearrangementProcedures(text: string) {
+  const { regexp } = parseRearrangementProcedures;
+  for (const [, quantityText, source, destination] of text.matchAll(regexp)) {
+    yield { quantity: parseInt(quantityText), source, destination };
+  }
+}
+
+function parseInput(text: string) {
+  const [topText, bottomText] = text.split("\n\n");
+  const startingStacks = parseStartingStacks(topText);
+  const rearrangementProcedures = parseRearrangementProcedures(bottomText);
+  return { startingStacks, rearrangementProcedures };
+}
+
+export default function solve(input: string) {
+  const { startingStacks: stacks, rearrangementProcedures } = parseInput(input);
+  for (const { quantity, source, destination } of rearrangementProcedures) {
+    stacks.get(destination)!.push(...stacks.get(source)!.splice(-quantity));
+  }
+  return Array.from(stacks.values(), (crates) => crates.at(-1)).join("");
+}
