9734e1e0b13e94b554adba455531f0e465684f56
Mark Fulton <mfulton26@gmail.com>
2024-01-07 11:18:52 -0600

    feat(intern): document

--- lib/intern.ts
+++ lib/intern.ts
@@ -5,6 +5,13 @@ const finalizationRegistry = new FinalizationRegistry((heldValue) =>
   internedWeakRefs.delete(heldValue)
 );
 
+/**
+ * Return the specified value or a previously interned value if its JSON string
+ * representation is the same.
+ * @see [Interning (computer science) - Wikipedia](https://en.wikipedia.org/wiki/Interning_(computer_science))
+ * @param value the value to intern
+ * @returns `value` if no existing value matches, otherwise the existing value
+ */
 export default function intern<T extends object>(value: T): T {
   const id = JSON.stringify(value);
   const internedValue = internedWeakRefs.get(id)?.deref();
