12d50fa15cfd9580edd2366b2c1d19590401bd1b
Mark Fulton <mfulton26@gmail.com>
2024-11-29 12:03:06 -0600

    style(2022/day/25): remove commented out code

--- 2022/day/25/part/1/solve.test.ts
+++ 2022/day/25/part/1/solve.test.ts
@@ -2,15 +2,6 @@ import solve from "./solve.ts";
 
 import { assertEquals } from "../../../../../lib/testing/asserts.ts";
 
-// Deno.test("one_line_input_1", expect(undefined));
-// Deno.test("one_line_input_N", expect(undefined));
-
-// function expect(expected: unknown) {
-//   return (t: Deno.TestContext) => {
-//     assertEquals(solve(t.name), expected);
-//   };
-// }
-
 Deno.test("example", () => {
   const input = `\
 1=-0-2
