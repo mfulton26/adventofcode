f268437be698e7887fda47629e5f98dd01d5ec7b
Mark Fulton <mfulton26@gmail.com>
2024-12-07 12:13:04 -0600

    refactor(2024/day/6): cleanup

--- 2024/day/6/part/1/solve.ts
+++ 2024/day/6/part/1/solve.ts
@@ -1,14 +1,15 @@
 export default function solve(input: string) {
   const grid = input.split("\n").reverse().map((line) => [...line]);
-  const { length: height, 0: { length: width } } = grid;
-  let [x, y] = grid.keys().flatMap((y) => grid[y].keys().map((x) => [x, y]))
+  const [x0, y0] = grid.keys().flatMap((y) => grid[y].keys().map((x) => [x, y]))
     .find(([x, y]) => grid[y][x] === "^")!;
-  let dx = 0, dy = 1;
   const seen = new Set<string>();
-  while (x >= 0 && x < width && y >= 0 && y < height) {
-    seen.add(`${x},${y}`);
+  for (
+    let x = x0, y = y0, dx = 0, dy = 1;
+    y in grid && x in grid[y];
+    x += dx, y += dy
+  ) {
     while (grid[y + dy]?.[x + dx] === "#") [dx, dy] = [dy, -dx];
-    x += dx, y += dy;
+    seen.add(`${x},${y}`);
   }
   return seen.size;
 }
--- 2024/day/6/part/2/solve.ts
+++ 2024/day/6/part/2/solve.ts
@@ -1,39 +1,36 @@
-// todo: reduce code duplication
-// todo: find a faster solution
-
-function loops(grid: string[][]) {
-  const { length: height, 0: { length: width } } = grid;
-  let [x, y] = grid.keys().flatMap((y) => grid[y].keys().map((x) => [x, y]))
-    .find(([x, y]) => grid[y][x] === "^")!;
-  let dx = 0, dy = 1;
-  const traveled = new Set<string>();
-  while (x >= 0 && x < width && y >= 0 && y < height) {
-    const hash = `${x},${y},${dx},${dy}`;
-    if (traveled.has(hash)) return true;
-    traveled.add(hash);
-    while (grid[y + dy]?.[x + dx] === "#") [dx, dy] = [dy, -dx];
-    x += dx, y += dy;
-  }
-  return false;
-}
-
 export default function solve(input: string) {
   const grid = input.split("\n").reverse().map((line) => [...line]);
-  const { length: height, 0: { length: width } } = grid;
-  let [x, y] = grid.keys().flatMap((y) => grid[y].keys().map((x) => [x, y]))
+  const [x0, y0] = grid.keys().flatMap((y) => grid[y].keys().map((x) => [x, y]))
     .find(([x, y]) => grid[y][x] === "^")!;
-  let dx = 0, dy = 1;
   const seen = new Set<string>();
-  while (x >= 0 && x < width && y >= 0 && y < height) {
-    seen.add(`${x},${y}`);
+  for (
+    let x = x0, y = y0, dx = 0, dy = 1;
+    y in grid && x in grid[y];
+    x += dx, y += dy
+  ) {
     while (grid[y + dy]?.[x + dx] === "#") [dx, dy] = [dy, -dx];
-    x += dx, y += dy;
+    seen.add(`${x},${y}`);
   }
   let count = 0;
   for (const hash of seen) {
     const [x, y] = hash.split(",").map(Number);
     if (grid[y][x] === "^") continue;
-    if (loops(grid.with(y, grid[y].with(x, "#")))) count++;
+    if (loops(grid.with(y, grid[y].with(x, "#")), x0, y0)) count++;
   }
   return count;
+}
+
+function loops(grid: string[][], x0: number, y0: number) {
+  const traveled = new Set<string>();
+  for (
+    let x = x0, y = y0, dx = 0, dy = 1;
+    y in grid && x in grid[y];
+    x += dx, y += dy
+  ) {
+    while (grid[y + dy]?.[x + dx] === "#") [dx, dy] = [dy, -dx];
+    const hash = `${x},${y},${dx},${dy}`;
+    if (traveled.has(hash)) return true;
+    traveled.add(hash);
+  }
+  return false;
 }
