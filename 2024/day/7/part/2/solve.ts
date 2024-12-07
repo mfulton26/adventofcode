f1f81b01c7d1d2cbeac5abcecd819c4930397da1
Mark Fulton <mfulton26@gmail.com>
2024-12-07 15:24:28 -0600

    feat: 2024 Day 7

---
+++ 2024/day/7/part/2/solve.ts
@@ -0,0 +1,24 @@
+export default function solve(input: string) {
+  const equations = input.split("\n").map((line) => {
+    const [left, right] = line.split(": ");
+    const testValue = +left;
+    const numbers = right.split(" ").map(Number);
+    return { testValue, numbers };
+  });
+  return equations
+    .filter(({ testValue, numbers }) => {
+      const stack = [numbers];
+      while (stack.length) {
+        const numbers = stack.pop()!;
+        if (numbers.length === 1) {
+          if (numbers[0] === testValue) return true;
+          else continue;
+        }
+        const [a, b, ...rest] = numbers;
+        stack.push([a + b, ...rest]);
+        stack.push([a * b, ...rest]);
+        stack.push([a * 10 ** `${b}`.length + b, ...rest]);
+      }
+    })
+    .reduce((sum, { testValue }) => sum + testValue, 0);
+}
