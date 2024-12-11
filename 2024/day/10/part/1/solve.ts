cbabf2501185836f4e6fd71c1edee03b3dcf47cd
Mark Fulton <mfulton26@gmail.com>
2024-12-10 19:00:15 -0600

    feat: 2024 Day 10

---
+++ 2024/day/10/part/1/solve.ts
@@ -0,0 +1,26 @@
+const directions = [[1, 0], [0, 1], [-1, 0], [-0, -1]];
+
+export default function solve(input: string) {
+  const map = input.split("\n").map((line) => Array.from(line, Number));
+  let sum = 0;
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] !== 0) continue;
+      const reachableHashes = new Set();
+      const stack = [[x, y]];
+      while (stack.length) {
+        const [x, y] = stack.pop()!;
+        const level = map[y][x];
+        if (level === 9) {
+          reachableHashes.add(`${x},${y}`);
+          continue;
+        }
+        for (const [dx, dy] of directions) {
+          if (map[y + dy]?.[x + dx] === level + 1) stack.push([x + dx, y + dy]);
+        }
+      }
+      sum += reachableHashes.size;
+    }
+  }
+  return sum;
+}
---
+++ 2024/day/10/part/2/solve.test.ts
@@ -0,0 +1,17 @@
+import solve from "./solve.ts";
+
+import { assertEquals } from "../../../../../lib/testing/asserts.ts";
+
+Deno.test("example", () => {
+  const input = `\
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`;
+
+  assertEquals(solve(input), 81);
+});
