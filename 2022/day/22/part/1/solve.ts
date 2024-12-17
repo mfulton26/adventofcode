23e33db5db6cba50831b720f9a8cceaf5e2ad949
Mark Fulton <mfulton26@gmail.com>
2024-12-16 21:32:22 -0600

    refactor(2022/day/22): simplify x,y

--- 2022/day/22/part/1/solve.ts
+++ 2022/day/22/part/1/solve.ts
@@ -1,43 +1,41 @@
-type Point = Readonly<{ x: number; y: number }>;
-
 function parseInput(text: string) {
   const [boardText, pathDescription] = text.split("\n\n");
   const board = boardText.split("\n")
-    .map((line) => Array.from(line, (char) => char === " " ? undefined : char));
+    .map((line) => Array.from(line, (char) => char.trim()));
   const path = pathDescription.matchAll(/\d+|L|R/g).map(([step]) => step);
   const x = board[0].findIndex(Boolean);
-  return { board, path, start: <Point> { x, y: 0 } };
+  return { board, path, start: [x, 0] };
 }
 
 export default function solve(input: string) {
   const { board, path, start } = parseInput(input);
   const { length: height, 0: { length: width } } = board;
-  let p = start, d = { x: 1, y: 0 };
+  let [px, py] = start, dx = 1, dy = 0;
   for (const step of path) {
     switch (step) {
       case "L":
-        d = { x: d.y, y: -d.x };
+        [dx, dy] = [dy, -dx];
         break;
       case "R":
-        d = { x: -d.y, y: d.x };
+        [dx, dy] = [-dy, dx];
         break;
       default: {
         const moves = Number(step);
         for (let n = 0; n < moves; n++) {
-          let next = { ...p };
+          let nx = px, ny = py;
           do {
-            next = { x: (next.x + d.x) % width, y: (next.y + d.y) % height };
-            if (d.x && next.x < 0) next.x += width;
-            if (d.y && next.y < 0) next.y += height;
-          } while (board[next.y][next.x] === undefined);
-          if (board[next.y][next.x] === "#") break;
-          p = next;
+            nx = (nx + dx) % width, ny = (ny + dy) % height;
+            if (dx && nx < 0) nx += width;
+            if (dy && ny < 0) ny += height;
+          } while (board[ny][nx] === undefined);
+          if (board[ny][nx] === "#") break;
+          [px, py] = [nx, ny];
         }
       }
     }
   }
-  const row = p.y + 1;
-  const column = p.x + 1;
-  const facing = (d.x === -1 || d.y === -1 ? 2 : 0) + (d.x === 0 ? 1 : 0);
+  const row = py + 1;
+  const column = px + 1;
+  const facing = (dx === -1 || dy === -1 ? 2 : 0) + (dx === 0 ? 1 : 0);
   return 1_000 * row + 4 * column + facing;
 }
