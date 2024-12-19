3de357d8b7bfc361fc2ce80fb37e80a2d6c04113
Mark Fulton <mfulton26@gmail.com>
2024-12-19 14:37:36 -0600

    perf(2024/day/19): leverage Set
    from 55ms down to 9ms

--- 2024/day/19/part/2/solve.ts
+++ 2024/day/19/part/2/solve.ts
@@ -5,13 +5,14 @@ export default function solve(input: string) {
   const designs = bottom.split("\n");
   let possibleCount = 0;
   const isPossibleRegExp = new RegExp(`^(?:${patterns.join("|")})+$`);
+  const patternsSet = new Set(patterns);
   for (const design of designs) {
     if (!isPossibleRegExp.test(design)) continue;
     const counts = Array.from({ length: design.length + 1 }, () => 0);
     counts[0] = 1;
     for (let j = 1; j <= design.length; j++) {
       for (let i = Math.max(0, j - maxPatternLength); i < j; i++) {
-        if (patterns.includes(design.slice(i, j))) counts[j] += counts[i];
+        if (patternsSet.has(design.slice(i, j))) counts[j] += counts[i];
       }
     }
     possibleCount += counts.at(-1)!;
