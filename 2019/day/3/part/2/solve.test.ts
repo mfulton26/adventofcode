bce1aadc3a658b7271e65957367e04a73c210117
Mark Fulton <mfulton26@gmail.com>
2025-11-28 10:19:05 -0600

    test(2019/day/3): correct test input

--- 2019/day/3/part/2/solve.test.ts
+++ 2019/day/3/part/2/solve.test.ts
@@ -3,26 +3,25 @@ import solve from "./solve.ts";
 import { assertEquals } from "@std/assert";
 
 Deno.test(
-  `\
+  "first example",
+  () => {
+    const input = `\
 R8,U5,L5,D3
-U7,R6,D4,L4`,
-  expect(30),
-);
-Deno.test(
-  `\
-R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83`,
-  expect(610),
-);
-Deno.test(
-  `\
-R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`,
-  expect(410),
+U7,R6,D4,L4`;
+    assertEquals(solve(input), 30);
+  },
 );
 
-function expect(expected: unknown) {
-  return (t: Deno.TestContext) => {
-    assertEquals(solve(t.name), expected);
-  };
-}
+Deno.test("second example", () => {
+  const input = `\
+R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83`;
+  assertEquals(solve(input), 610);
+});
+
+Deno.test("third example", () => {
+  const input = `\
+R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`;
+  assertEquals(solve(input), 410);
+});
