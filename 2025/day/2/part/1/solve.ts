760ade77d8eb1103a00bd18068dec48f92d74660
Mark Fulton <mfulton26@gmail.com>
2025-12-02 06:59:04 -0600

    perf(2025/day/2): calculate repeats
    instead of iterating through all IDs and checking for repeats the repeated digits can be calculated

--- 2025/day/2/part/1/solve.ts
+++ 2025/day/2/part/1/solve.ts
@@ -1,13 +1,16 @@
-const regExp = /^(\d+?)\1$/;
-
 export default function solve(input: string) {
   const ranges = input.split(",").map((line) => line.split("-").map(Number));
   let sum = 0;
-  for (const [first, last] of ranges) {
-    for (let id = first; id <= last; id++) {
-      if (regExp.test(`${id}`)) {
-        sum += id;
-      }
+  for (const [firstId, lastId] of ranges) {
+    const firstIdLength = Math.floor(Math.log10(firstId) + 1);
+    const divisor = 10 ** Math.ceil(firstIdLength / 2);
+    const lower = Math.floor(firstId / divisor);
+    const upper = Math.floor(lastId / divisor);
+    for (let digits = lower; digits <= upper; digits++) {
+      const id = +`${digits}${digits}`;
+      if (id < firstId) continue;
+      if (id > lastId) break;
+      sum += id;
     }
   }
   return sum;
--- 2025/day/2/part/2/solve.ts
+++ 2025/day/2/part/2/solve.ts
@@ -1,11 +1,21 @@
-const regExp = /^(\d+?)\1+$/;
-
 export default function solve(input: string) {
   const ranges = input.split(",").map((line) => line.split("-").map(Number));
   let sum = 0;
-  for (const [first, last] of ranges) {
-    for (let id = first; id <= last; id++) {
-      if (regExp.test(`${id}`)) {
+  for (const [firstId, lastId] of ranges) {
+    const firstIdLength = Math.floor(Math.log10(firstId) + 1);
+    const lastIdLength = Math.ceil(Math.log10(lastId) + 1);
+    const seen = new Set<number>();
+    for (let repeatCount = 2; repeatCount <= lastIdLength; repeatCount++) {
+      const divisor = 10 **
+        Math.ceil(firstIdLength * (repeatCount - 1) / repeatCount);
+      const lower = Math.floor(firstId / divisor);
+      const upper = Math.floor(lastId / divisor);
+      for (let digits = lower; digits <= upper; digits++) {
+        const id = +`${digits}`.repeat(repeatCount);
+        if (id < firstId) continue;
+        if (id > lastId) break;
+        if (seen.has(id)) continue;
+        seen.add(id);
         sum += id;
       }
     }
