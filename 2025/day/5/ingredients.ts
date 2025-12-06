377ecf0cf3e5c54b0d6927b879e29747a36ccbf6
Mark Fulton <mfulton26@gmail.com>
2025-12-06 13:26:58 -0600

    refactor(2025/day/5/ingredients): use regexp

--- 2025/day/5/ingredients.ts
+++ 2025/day/5/ingredients.ts
@@ -1,9 +1,8 @@
 export function parseDatabase(text: string) {
   const [top, bottom] = text.split("\n\n");
-  const freshIdRanges = top.split("\n").map((text: string) => {
-    const [start, end] = text.split("-").map(Number);
-    return { start, end };
-  });
+  const freshIdRanges = top.matchAll(/(?<start>\d+)-(?<end>\d+)/g)
+    .map(({ groups: { start, end } = {} }) => ({ start: +start, end: +end }))
+    .toArray();
   const availableIds = bottom.split("\n").map(Number);
   return { freshIdRanges, availableIds };
 }
