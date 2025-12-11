a6c74b5070441294ec9e1682f4db99ce38a5bef6
Mark Fulton <mfulton26@gmail.com>
2025-12-11 07:26:54 -0600

    perf(2025/day/11/part/1): reuse `countPaths()`
    there's no need to actually find the paths, only the counts of them

--- 2025/day/11/part/1/solve.ts
+++ 2025/day/11/part/1/solve.ts
@@ -1,14 +1,7 @@
 import { parseDevices } from "../../devices.ts";
+import { countPaths } from "../../paths.ts";
 
 export default function solve(input: string) {
   const devices = parseDevices(input);
-  const stack = [["you"]];
-  let pathCount = 0;
-  while (stack.length) {
-    const path = stack.pop()!;
-    const end = path.at(-1)!;
-    if (end === "out") pathCount++;
-    else devices.get(end)?.forEach((output) => stack.push([...path, output]));
-  }
-  return pathCount;
+  return countPaths(devices, "you", "out");
 }
