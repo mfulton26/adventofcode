996e839b929bcba93511ec262f30e195716a30b3
Mark Fulton <mfulton26@gmail.com>
2025-12-15 11:41:49 -0600

    refactor(2025/day/7): renaming some variables

--- 2025/day/7/part/2/solve.ts
+++ 2025/day/7/part/2/solve.ts
@@ -1,18 +1,17 @@
 export default function solve(input: string) {
   const rows = input.split("\n").map((line) => Array.from(line));
-  const beamColumnIndexes = new Map([[rows[0].indexOf("S"), 1]]);
+  const timelineCounts = new Map([[rows[0].indexOf("S"), 1]]);
   for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
-    for (const [beamColumnIndex, timelineCount] of beamColumnIndexes) {
+    for (const [beamColumnIndex, timelineCount] of timelineCounts) {
       if (rows[rowIndex + 1]?.[beamColumnIndex] !== "^") continue;
-      beamColumnIndexes.delete(beamColumnIndex);
+      timelineCounts.delete(beamColumnIndex);
       for (const direction of [-1, 1]) {
         const splitColumnIndex = beamColumnIndex + direction;
-        const prevTimelineCount = beamColumnIndexes.get(splitColumnIndex);
+        const prevTimelineCount = timelineCounts.get(splitColumnIndex);
         const nextTimelineCount = (prevTimelineCount ?? 0) + timelineCount;
-        beamColumnIndexes.set(splitColumnIndex, nextTimelineCount);
+        timelineCounts.set(splitColumnIndex, nextTimelineCount);
       }
     }
   }
-  return beamColumnIndexes.values()
-    .reduce((sum, timelineCount) => sum + timelineCount, 0);
+  return timelineCounts.values().reduce((sum, count) => sum + count, 0);
 }
