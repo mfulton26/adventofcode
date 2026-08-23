01c94ab737cead7732a086e3b53de59682d27460
Mark Fulton <mfulton26@gmail.com>
2026-08-23 17:55:48 -0500

    chore: update test template
    `Deno.test.each()` is easily adopted for parameterized tests now

--- :year/day/:day/part/1/solve.test.ts
+++ :year/day/:day/part/1/solve.test.ts
@@ -2,17 +2,9 @@ import solve from "./solve.ts";
 
 import { assertEquals } from "@std/assert";
 
-Deno.test("one_line_input_1", expect(undefined));
-Deno.test("one_line_input_N", expect(undefined));
-
-function expect(expected: unknown) {
-  return (t: Deno.TestContext) => {
-    assertEquals(solve(t.name), expected);
-  };
-}
-
 Deno.test("example", () => {
-  const input = ``;
+  const input = `\
+`;
 
   assertEquals(solve(input), undefined);
 });
